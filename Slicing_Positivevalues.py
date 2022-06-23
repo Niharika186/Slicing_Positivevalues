0a78f173eead8b666025a09830e124d5e2178b26
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-06-23 11:04:03 +0530

    Add files via upload

---
+++ Slicing_Positivevalues.py
@@ -0,0 +1,26 @@
+# a='niharika'
+# print(a[ : ]) # niharika (if we don't gave the value in right side it will show the total string)
+# print(a[1:]) # iharika
+# print(a[2:]) # harika
+# print(a[3:]) # arika
+# print(a[4:]) # rika
+# print(a[5:]) # ika
+# print(a[6:]) # ka
+# print(a[7:]) # a
+
+# print(a[:8]) # niharika (if we gave the value in right side it will reduce one value & in left side it takes the value by defaul as 0)
+# print(a[:7]) # niharik
+# print(a[:6]) # nihari
+# print(a[:5]) # nihar
+# print(a[:4]) # niha
+# print(a[:3]) # nih
+# print(a[:2]) # ni
+# print(a[:1]) # n
+
+# print(a[0:5]) # nihar ( left side value should be less than the right side value)
+# print(a[5:8]) # ika
+
+# print(a[4:3]) # empty line( if the left sid value is greater than right side value it not shows the error,bt it shows empty line )
+# print(a[6:7]) # empty line
+# print(a[4:4]) # empty line
+# print(a[8:]) # empty line
